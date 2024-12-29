8a7de57fd9c9f78386c20cf98edf7d175bd9c9c7
Ethan Bertin--Prévot <ethan.bertin-prevot@epitech.eu>
2024-12-29 18:33:01 +0100

    Put stuff in their own files - 'pass'.

---
+++ api.js
@@ -0,0 +1,43 @@
+//api.js
+
+ftch = document.getElementById("fetch");
+btn = document.getElementById("button");
+
+async function tell_backend_button_click() {
+
+    fetch('https://yknottyd.duckdns.org/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({request: "button_click"}),
+    }).then((response) => {
+        return response.text()
+    }).then(data => {
+        ftch.textContent = data;
+    })
+}
+
+async function ask_backend_button_clicks() {
+
+    fetch('https://yknottyd.duckdns.org/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({request: "ask"}),
+    }).then((response) => {
+        return response.text()
+    }).then(data => {
+        ftch.textContent = data;
+    })
+}
+
+async function ask_backend_clicks_loop() {
+    while (1) {
+        ask_backend_button_clicks();
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+}
+
+ask_backend_clicks_loop();
